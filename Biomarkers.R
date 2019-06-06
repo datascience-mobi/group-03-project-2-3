8a2bb4ec212e536bba6a7f42663a59fe7fec20be
Diego <digomsolano@hotmail.com>
2019-06-06 11:35:44 +0200

    Final Biomarkers
    Completed analysis and list of the 15 biomarkers chosen for Gemcitabine

--- Biomarkers.R
+++ Biomarkers.R
@@ -47,9 +47,16 @@ orderforvectors <- function(x) {
 matrixofbiomarkers <- apply(absvalfcge_gemcitabine, 2, orderforvector) #the 2 means it is applied to columns
 
 
-#Check for most common genes in the first 20 (?) rows of the new matrix -> biomarkers!
-topgenes <- sort(table(as.vector(matrixofbiomarkers[1:20,])))
+#Check for most common genes in the first 15 rows of the new matrix -> biomarkers!
+topgenes <- sort(table(as.vector(matrixofbiomarkers[1:15,])))
+
+#Convert topgenes into a matrix in descending order of counts
+matrixtopgenes <- as.matrix(topgenes[order(-topgenes)])
 
 #Bar plot of topgenes to find the biomarkers
+barplot(matrixtopgenes[1:20,], xlab = "Genes", ylab = "Counts", main = "Top differentially expressed genes")
+#We decided to take the top 15 genes as our biomarkers
 
+#List of our biomarkers
+biomarkers <- names(matrixtopgenes[1:15,1])
 
